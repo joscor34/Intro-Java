6873513cc04c4db46961c77c93d0699134541353
Jose Eduardo Esteva Cordova <zbrm34@gmail.com>
2018-04-30 17:09:49 -0500

    Funcion del If y el Else

---
+++ proyectos/If.java
@@ -0,0 +1,21 @@
+
+public class If {
+
+	public static void main(String[] args) {
+    int a = 10;
+    int b = 5;
+    int g = 7;
+    int c;
+    c = a + b;
+    if(c==15) {
+    	System.out.println("Si funciona");
+    	for(int i=0;i<=100;i++) {
+    	  System.out.println("'i' vale "+i);
+    	  }
+      }
+    else {
+    	System.out.println("Nel no funciona");
+        }
+	}
+
+}

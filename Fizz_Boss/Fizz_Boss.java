cc197eff22d00e82d82a0cfda46444d3cb2234bd
Jose Eduardo Esteva Cordova <zbrm34@gmail.com>
2018-05-02 17:42:22 -0500

    Ejercicio Fizz_Boss Invierte

---
+++ Fizz_Boss/Fizz_Boss.java
@@ -0,0 +1,20 @@
+
+public class Fizz_Boss {
+
+	public static void main(String[] args) {
+	int x;
+		for(x = 0;x<=100;x++) {
+		if(x % 2 == 0) {
+		System.out.println("Fizz");	
+		}
+		if(x % 5 == 0) {
+		
+			System.out.println("Boss");
+		}
+		if(x % 2 == 0 && x % 5 == 0) {
+			System.out.println("Fizz Boss");
+		}
+			
+		}
+		}
+	
